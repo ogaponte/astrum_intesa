f8e41a3bd232c3c707c521eaf6241a11ca13b362
Oscar Aponte Aguilar <oaponte@vmi2121004.contaboserver.net>
2025-03-21 13:17:14 -0500

    refractor: Estructura base

--- __manifest__.py
+++ __manifest__.py
@@ -1,34 +1,25 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "astrum_intesa",
+    'name': "Astrum Intesa",
 
-    'summary': "Short (1 phrase/line) summary of the module's purpose",
+    'summary': "Módulo de integración con Intesa",
 
     'description': """
-Long description of module's purpose
+Módulo de integración con Intesa",
     """,
 
-    'author': "My Company",
-    'website': "https://www.yourcompany.com",
+    'author': "Astrum Consultoría y Gestión",
+    'website': "https://www.astrum-cg.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
+    'category': 'productivity',
+    'version': '1.0',
+    'license': 'LGPL-3',
 
-    # any module necessary for this one to work correctly
-    'depends': ['base'],
+    'depends': ['base','purchase','account'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
-    ],
+    ]
 }
 
